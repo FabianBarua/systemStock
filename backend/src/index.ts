3be5666ff6e7b810be0339d9c73036b8b3a44aaa
Fabian <baruafabian@gmail.com>
2024-07-14 23:06:10 -0400

    chore: Update backend configuration and dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,13 +6,10 @@ import api from './api/index'
 config()
 
 const app = express()
-const port = process.env.BACK_PORT || 3000
 
 app.use(cors()) // Habilita CORS para todas las rutas y orígenes
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api/', api)
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
-})
+export default app
