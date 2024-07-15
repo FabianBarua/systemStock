e8b6a14203ec3de25bf8010981f234909bcc2bc5
Fabian <baruafabian@gmail.com>
2024-07-14 23:11:36 -0400

    chore: Add endpoint for root URL and server startup message

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,5 +11,11 @@ app.use(cors()) // Habilita CORS para todas las rutas y orígenes
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api/', api)
+app.use('/', (req, res) => res.send('Hello World!'))
 
 export default app
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
